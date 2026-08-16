d89a2343b167445f1d12bba5db2213003e595a34
pauel <pau.lacazedieu@gmail.com>
2026-08-16 10:04:39 +0200

    update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ -d "/home/steam/plugins" ]; then
     chown -R steam:steam "/home/steam/plugins"
 fi
@@ -16,12 +16,10 @@ cp -r /home/steam/plugins "$SERVER_DIR/BepInEx/plugins"
 cd "$SERVER_DIR" || exit 1
 
 # Default framerate to 120 if null. Mainly for backwards compatibility of older versions
-# shellcheck disable=SC3010
 if [[ -z $FRAMERATE ]]; then
     FRAMERATE=120
 fi
 
-# shellcheck disable=SC3010
 if [[ $ENABLE_LOGS == true ]]; then
     exec ./run_bepinex.sh NuclearOptionServer.x86_64 -limitframerate "$FRAMERATE" -logFile "./logs/server-$(date +%Y-%m-%d-%H-%M-%S).log"
 else
